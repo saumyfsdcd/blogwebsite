fe4018a50debeb7fb8c108da12a1ecb8f4fdcdfc
saumyfsdcd <samcode46@gmail.com>
2023-05-28 11:28:35 +0530

    D

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post("/", (req,res)=>{
     res.redirect("/");
 })
 
-const port=process.env.port || 3000;
+const port=process.env.PORT || 3000;
 
 app.listen(port, ()=>{
     console.log("server started on port" + port)
